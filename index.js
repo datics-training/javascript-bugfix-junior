dc9ef12b21bcfe814581af260ff547272c58c9a2
Datmaniac <30699799+avaritia95@users.noreply.github.com>
2022-11-07 11:47:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Create a function that returns true if two arrays contain identical values, and false otherwise.
+
+// The following code given
+
+// Please test the code with these test cases:
+// arr1 = [1, 2] and arr2 = [1, 3]
+// arr2 = [1, 2] and arr2 = [1, 2]
+
+// Explain the problem with the code, and make necessary correction to fulfill the requirement 
+
+function checkEquals(arr1, arr2) {
+  if (arr1 === arr2) {
+    return true
+  } else {
+    return false
+  }
+}
