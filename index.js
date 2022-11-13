9d9fec4f5303ff8a3ad4a84678a68c2f63c6d551
mbarisyaman <>
2022-11-13 14:52:50 +0000

    Adjust checkEqual function to compare arrays

--- index.js
+++ index.js
@@ -9,9 +9,22 @@
 // Explain the problem with the code, and make necessary correction to fulfill the requirement 
 
 function checkEquals(arr1, arr2) {
-  if (arr1 === arr2) {
-    return true
-  } else {
-    return false
+  if(arr1.length !== arr2.length){
+    return false;
   }
+  for(let i = 0; i<arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
 }
+
+const arr1 = [1, 2];
+const arr2 = [1, 3];
+
+const arr3 = [1, 2];
+const arr4 = [1, 2];
+
+console.log(checkEquals(arr1, arr2));
+console.log(checkEquals(arr3, arr4));
